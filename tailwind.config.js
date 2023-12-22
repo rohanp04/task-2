b675682ec1bcf0f6b553f5267af208af03539ae4
rohanp04 <139552770+rohanp04@users.noreply.github.com>
2023-12-22 16:27:02 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    // Specify the files that should be scanned for classes
-    content: [
-      './dist/**/*.html',
-      // Add any other file types or paths that include your Tailwind CSS classes
-    ],
-  },
+  
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {},
